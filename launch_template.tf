8648fcba6238d25cec45dd98e5c54caf9680a8e5
kmh0831 <kmhyuk1018@gmail.com>
2024-08-02 12:41:43 +0900

    TEST

--- launch_template.tf
+++ launch_template.tf
@@ -1,11 +1,18 @@
+resource "aws_iam_instance_profile" "example_launch" {
+  count = data.aws_iam_instance_profile.existing_codedeploy_instance_profile.name != "" ? 0 : 1
+
+  name = "example_instance_profile"
+  role = aws_iam_role.codepipeline_role[count.index].name
+}
+
 resource "aws_launch_template" "example" {
   name = "example-launch-template"
 
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.ec2_key.key_name # 실제 키 페어 이름으로 변경해야 합니다.
+  key_name      = aws_key_pair.ec2_key.key_name
 
   iam_instance_profile {
-    name = aws_iam_instance_profile.example[0].name
+    name = aws_iam_instance_profile.example_launch[count.index].name
   }
 
   user_data = <<-EOF
@@ -25,17 +32,3 @@ resource "aws_launch_template" "example" {
     Name = "example-instance"
   }
 }
-
-# 데이터 소스: 기존 IAM 인스턴스 프로필 확인
-data "aws_iam_instance_profile" "existing_codedeploy_instance_profile" {
-  name = "codedeploy_instance_profile"
-}
-
-# IAM 인스턴스 프로필이 존재하지 않는 경우에만 새로 생성
-resource "aws_iam_instance_profile" "example_launch" {
-  count = data.aws_iam_instance_profile.existing_codedeploy_instance_profile.name != "" ? 0 : 1
-
-  name = "example_instance_profile"
-  role = aws_iam_role.example.name
-}
-
