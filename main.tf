869fef938ac566741e5ee7962960b91c80ebc243
kmh0831 <kmhyuk1018@gmail.com>
2024-08-02 16:43:53 +0900

    v1

--- main.tf
+++ main.tf
@@ -4,55 +4,55 @@ provider "aws" {
 
 # CodeCommit repository
 module "codecommit" {
-  source = "./modules/codecommit"
+  source = "./modules/codecommit.tf"
 }
 
 # CodeBuild project
 module "codebuild" {
-  source = "./modules/codebuild"
+  source = "./modules/codebuild.tf"
 }
 
 # CodeDeploy application and deployment group
 module "codedeploy" {
-  source = "./modules/codedeploy"
+  source = "./modules/codedeploy.tf"
 }
 
 # CodePipeline
 module "codepipeline" {
-  source = "./modules/codepipeline"
+  source = "./modules/codepipeline.tf"
 }
 
 # EC2 Launch Template
 module "ec2_template" {
-  source = "./modules/ec2_template"
+  source = "./modules/ec2_template.tf"
 }
 
 # IAM roles and policies
 module "iam" {
-  source = "./modules/iam"
+  source = "./modules/iam.tf"
 }
 
 # VPC
 module "vpc" {
-  source = "./modules/vpc"
+  source = "./modules/vpc.tf"
 }
 
 # ALB
 module "alb" {
-  source = "./modules/alb"
+  source = "./modules/alb.tf"
 }
 
 # Autoscaling
 module "autoscaling" {
-  source = "./modules/autoscaling"
+  source = "./modules/autoscaling.tf"
 }
 
 # S3
 module "s3" {
-  source = "./modules/s3"
+  source = "./modules/s3.tf"
 }
 
 # EC2
 module "EC2" {
-  source = "./modules/web-ec2"
+  source = "./modules/web-ec2.tf"
 }
