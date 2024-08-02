92ed9c18b76159b86a70082d3db5177ee7f6364d
kmh0831 <kmhyuk1018@gmail.com>
2024-08-02 12:06:54 +0900

    v1

--- codebuild.tf
+++ codebuild.tf
@@ -1,6 +1,6 @@
 resource "aws_codebuild_project" "build_project" {
   name         = "airline-booking-build"
-  service_role = aws_iam_role.codebuild_role.arn
+  service_role = aws_iam_role.codebuild_role[0].arn  # 인덱스 사용
 
   artifacts {
     type      = "S3"
